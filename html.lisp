3133133dee9ccd2a5a744b4058276242490cdc70
Aad Versteden <madnificent@gmail.com>
2008-12-30 14:26:17 +0100

    import of initial html-generator

---
+++ html.lisp
@@ -0,0 +1,38 @@
+;;;;  This is the micro html-generation-library.  It is possible to make this more efficiently by using functions, but that can be implemented later.
+
+(defpackage minions.html
+  (:use :common-lisp))
+
+(in-package :minions.html)
+
+(defun tagify (keyword options &optional (contentp nil))
+  "Creates the printed tag for the given keyword"
+  (if contentp
+      (values (format nil "<~A~{ ~A=\"~A\"~}>" keyword options)
+	      (format nil "</~A>" keyword))
+      (format nil "<~A ~{~A=~A ~}/>" keyword options)))
+
+(defun htmlify (list)
+  "Transforms a sexp to an html document."
+  (labels ((key-vals (list) 
+	     (let ((content (rest list))
+		   (keys nil))
+	       (loop while (keywordp (first content))
+		  do (progn (setf keys (concatenate 'list keys (list (first content) (second content))))
+			    (setf content (rest (rest content)))))
+	       (values keys content))))
+    (multiple-value-bind (keys content)
+	(key-vals list)
+      (multiple-value-bind (start-tag end-tag)
+	  (tagify (first list) keys content)
+	(if end-tag
+	    (format nil "~A~%~{~A~}~%~A" 
+		    start-tag
+		    (map 'list (lambda (x)
+				 (if (stringp x)
+				     x
+				     (htmlify x)))
+			 content)
+		    end-tag)
+	    start-tag)))))
+      
