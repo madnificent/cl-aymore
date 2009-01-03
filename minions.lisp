0fa4c226efc336a887c57694ad84cdeb5ee527e2
Aad Versteden <madnificent@gmail.com>
2009-01-03 19:22:32 +0100

    Created a page-definition-system which enforces users to give their pages a name.  This should make routing easier in the near future

--- minions.lisp
+++ minions.lisp
@@ -3,9 +3,14 @@
   (:export :defpage))
 (in-package :minions)
 
-(defmacro defpage (path &rest content)
-  (let ((function (if (listp (first content))
-		      `(lambda () ,@content)
-		      `(quote ,(first content)))))
-    `(push (create-prefix-dispatcher ,path ,function) *dispatch-table*)))
-      
+(defmacro defpage (name path &body content)
+  "Defines a page, and adds it to the dispatch-table"
+  (let ((statements nil))
+    (when (listp (first content)) ;; define the function if it is not a key
+      (push `(defun ,name () ,@content)
+	    statements))
+    ;; add the route
+    (push `(push (create-prefix-dispatcher ,path ',name)
+		 *dispatch-table*)
+	  statements)
+    `(progn ,@(reverse statements))))
