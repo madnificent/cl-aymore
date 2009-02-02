9d15c810ee4ba47e8009088fc9ea0b3051b1c703
MADnificent <madnificent@gmail.com>
2009-02-02 17:42:37 +0100

    second stage of the latest routing effort

--- routing.lisp
+++ routing.lisp
@@ -15,14 +15,22 @@
 ;;                       ("overview" overview)
 ;;                       ("status" status))
 ;;                      ("welcome" welcome)))
+;; (page-handler "about/contact") >> 'contact
+;;
 ;; h2. stage 2
 ;;  The url for a page will be creatable, based on the page (and possible some options). This will be for non-regexp pages.
+;;  example
+;; (handler-url 'contact) >> "about/contact"
+;;
 ;; h2. stage 3
 ;;  Pages will be able to parse the content in a URL, and this will allow them to define variables that the user can access.
+;;
 ;; h2. stage 4
 ;;  Pages with variables will be generatable. <eg: (get-page-url 'show-post :post-id 126)>
+;;
 ;; h2. stage 5
 ;;  Pages can contain abbreviations (this is the handles clause)
+;;
 ;; h2. stage 6
 ;;  Pages can contain functionality to define new variables (or logging, or whatever code they want to be executed in the current scope).
 
@@ -43,7 +51,6 @@
 
 (defun search-handler (route url-sections)
   "Searches for a handler for the current page in the currently known urls."
-  (util:debug-print route url-sections)
   (when (string= (first route) (first url-sections))
     (if (rest url-sections)
 	(dolist (subroute (rest route))
@@ -53,3 +60,43 @@
 		(return-from search-handler handler)))))
 	(if (or (symbolp (second route)) (functionp (second route)))
 	    (second route)))))
+
+(defun handler-url (page &rest options)
+  "Finds the url for a given page-handler.  The options can specify any number of (currently unspecified options)"
+  (dolist (route *ROUTING-TABLE*)
+    (let ((url (apply 'search-url route page options)))
+      (when url
+	(return-from handler-url url)))))
+
+(defun search-url (route page &rest options)
+  "Searches for a url of the given page, for the current (sub) route."
+  (if (eql (second route) page)
+      (first route)
+      (dolist (subroute (rest route))
+	(when (listp subroute)
+	  (let ((url (apply 'search-url subroute page options)))
+	    (when url
+	      (return-from search-url (concatenate 'string (first route) (string +URLSPLIT+) url))))))))
+		 
+;; (set-routing-table
+;;  ("admin"
+;;   'site.admin:overview
+;;   ("users"
+;;    'site.admin.users:index
+;;    ("" 'site.admin.users:index)
+;;    ("\d+" (handler identifies 'backend.users:user *user* by :id)
+;; 	  (handles 'site.admin.users:show when (minions:request-method :get))
+;; 	  (handles 'site.admin.users:create when (minions:request-method :put))
+;; 	  (handles 'site.admin.users:destroy when (minions:request-method :destroy))
+;; 	  ("edit" 'site.admin.users:edit when (minions:request-method :get)
+;; 		  'site.admin.users:update when (minions:request-method :put))
+;; 	  ("new" 'site.admin.users:new when (minions:request-method :get)
+;; 		 'site.admin.users:new when (mininos:request-method :put))))
+;;   ("tickets"
+;;    (handles 'site.ticketing:index loosely)
+;;    ("" 'site.ticketing:index)
+;;    (".*" (handler identifies 'backend.ticketing by :title)))
+;;   ("settings"
+;;    (handles 'site.config:index loosely)
+;;    ("config" (handles 'site.config:edit when (minions:request-method :get))
+;; 	     (handles 'site.config:commit when (minions:request-method :put))))))
