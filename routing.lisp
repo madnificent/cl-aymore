a2188347d5bca0bb6af22e879b8644faa87178f9
MADnificent <madnificent@gmail.com>
2009-02-02 17:26:38 +0100

    Stage one of the latest effort with regard to routing

--- routing.lisp
+++ routing.lisp
@@ -1,23 +1,8 @@
 (defpackage :minions.routing
   (:use :common-lisp
-	:cl-ppcre)
-  ;; handlers on node-objects
-  (:export :set-page-handler
-	   :page-handler
-	   :page-url
-	   :tableize)
-  ;; handlers on the general system
-  (:export :clear-table
-	   :get-page-handler
-	   :get-page-url
-	   :add-page
-	   :print-table))
+	:cl-ppcre))
 
 (in-package :minions.routing)
-
-(defvar *table* nil)
-(defvar *URL-VARS* nil)
-
 
 ;; The current idea is to work from the data structure the user has given us.  This means that that data structure will be the internal representation too.
 ;;
@@ -25,11 +10,11 @@
 ;; h2. stage 1
 ;;  Pages will be identifyable by the internal representation.  Only static pages will be considered.  That means that we can render a page from the hunchentoot.
 ;;  example
-;; (set-routing-table '(("about" 'about-overview
-;; 		      ("contact" 'contact)
-;; 		      ("overview" 'overview)
-;; 		      ("status" 'status))
-;; 		     ("welcome" 'welcome)))
+;; (set-routing-table '(("about" about-overview
+;;                       ("contact" contact)
+;;                       ("overview" overview)
+;;                       ("status" status))
+;;                      ("welcome" welcome)))
 ;; h2. stage 2
 ;;  The url for a page will be creatable, based on the page (and possible some options). This will be for non-regexp pages.
 ;; h2. stage 3
@@ -45,3 +30,26 @@
 
 (defvar *ROUTING-TABLE* nil)
 
+(defun set-routing-table (content)
+  (setf *ROUTING-TABLE* content))
+
+(defun page-handler (url)
+  "Finds the handler for a page in the current routing table."
+  (let ((parts (cl-ppcre:split (string +URLSPLIT+) url)))
+    (dolist (route *ROUTING-TABLE*)
+      (let ((handler (search-handler route parts)))
+	(when handler
+	  (return-from page-handler handler))))))
+
+(defun search-handler (route url-sections)
+  "Searches for a handler for the current page in the currently known urls."
+  (util:debug-print route url-sections)
+  (when (string= (first route) (first url-sections))
+    (if (rest url-sections)
+	(dolist (subroute (rest route))
+	  (when (listp subroute)
+	    (let ((handler (search-handler subroute (rest url-sections))))
+	      (when handler
+		(return-from search-handler handler)))))
+	(if (or (symbolp (second route)) (functionp (second route)))
+	    (second route)))))
