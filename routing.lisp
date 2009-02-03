9bf8ee18cb08483f1996d57a8b1a3beefdd8c9dc
Aad Versteden <madnificent@gmail.com>
2009-02-03 01:24:13 +0100

    Stage 5 is fininshed, new stages have been inserted.

--- routing.lisp
+++ routing.lisp
@@ -1,12 +1,20 @@
 (defpackage :minions.routing
   (:use :common-lisp
-	:cl-ppcre))
-
+	:cl-ppcre)
+  (:export :set-routing-table
+	   :page-handler
+	   :handler-url
+	   :defhandler
+	   :sets
+	   :url-var
+	   :defhandles
+	   :loosely))
+	   
 (in-package :minions.routing)
 
 ;; The current idea is to work from the data structure the user has given us.  This means that that data structure will be the internal representation too.
 ;;
-;; In order for this to work out nicely, the system will be built up in 6 stages.
+;; In order for this to work out nicely, the system will be built up in 7 stages.
 ;; h2. stage 1
 ;;  Pages will be identifyable by the internal representation.  Only static pages will be considered.  That means that we can render a page from the hunchentoot.
 ;;  example
@@ -31,13 +39,45 @@
 ;; (defpage show-post () (with-standard-page (h1 "Requested : " (url-var 'post-id))))
 ;;
 ;; h2. stage 4
-;;  Pages with variables will be generatable. <eg: (get-page-url 'show-post 'post-id 126)>
+;;  Pages with variables will be generatable.
+;; example:
+;;  (page-handler 'show-post 'post-id 126)
 ;;
 ;; h2. stage 5
 ;;  Pages can contain abbreviations (this is the handles clause)
+;; example:
+;;  (set-routing-table '(("posts" (handles posts loosely)))
+;;  expands to
+;;  (set-routing-table '(("posts" posts
+;;                        ("" posts))))
 ;;
 ;; h2. stage 6
-;;  Pages can contain functionality to define new variables (or logging, or whatever code they want to be executed in the current scope).
+;;  Pages can define conditional evaluation (when (condition) action).  Both (when ...) and (handles action when (condition)) should work.
+;; example:
+;;  (set-routing-table '(("posts" (handles post-list loosely)
+;;                        ("\\d+" (handler sets post-id)
+;;                         (when get-request show-post)
+;;                         (when put-request create-post)
+;;                         (when destroy-request destroy-post)))
+;;                       ("users" (handles users loosely)
+;;                        ("\\d+" (handler sets user-id)
+;;                         (handles show-user when get-request)
+;;                         (handles update-user when post-request)))))
+;;
+;; h2. stage 7
+;;  Operations for managing the routing table.  These operations are only basic, as the more complicated behavior can be done manually (readable-table) gives you a table that is editable.  Manipulating that list (which is easy) allows the system to be reconfigured based on the user's needs.
+;; example:
+;;  (move "foo/bar" "foo/baz")
+;;  (delete "foo/bar")
+;;  (insert "foo/bar" (subtable-list))
+;;  (update "foo/bar" (subtable-list)) ; this is a delete and an insert
+;;  (readable-table)
+;;
+;; h2. stage 8
+;;  Named subsections.  These named subsections can be updated without direct table manipulation.  This would provide better support for creating plugins that contain routes to manage the plugins.
+;; example:
+;;  (insert "foo/bar" 'posts-routing)
+;;  (update 'posts-routing '("posts" (handles post-list loosely) ("\\d+" (handler sets post-id) (when get-request show-post))))
 
 (defconstant +URLSPLIT+ #\/)
 
@@ -45,7 +85,34 @@
 (defvar *url-variables* nil)
 
 (defun set-routing-table (content)
-  (setf *ROUTING-TABLE* content))
+  (setf *ROUTING-TABLE* (expand-handles-cases content)))
+
+(defun expand-handles-cases (content)
+  (let ((resulting-list))
+    (mapc (lambda (item)
+	    (cond ((and (listp item) 
+			(eql (first item) 'handles))
+		   (setf resulting-list 
+			 (reverse 
+			  (expand-handles-cases 
+			   (reverse`(,@(reverse (apply (third item) (second item) (rest (rest (rest item))))) ,@resulting-list))))))
+		  ((listp item)
+		   (push (expand-handles-cases item) resulting-list))
+		  (T
+		   (push item resulting-list))))
+	  content)
+    (reverse resulting-list)))
+
+(defmacro defhandles (name (base &rest args) documentation &body body)
+  "Allows users to create a new clause for the handles expanders.
+   This will receive some content and must then return a new list, which will be spliced into the given routing table."
+  `(defun ,name (,base ,@args)
+     ,documentation
+     ,@body))
+
+(defhandles loosely (page)
+    "Allows a page to be linked both through foo and foo/, with foo being the predefined regexp in the routing table"
+  `(,page ("" ,page)))
 
 (defmacro with-parsing-environment (&body body)
   "Sets up the environment for the parsing of the url.  This will give you the basic space to store variables in etc."
@@ -73,7 +140,7 @@
 (defun search-handler (route url-sections)
   "Searches for a handler for the current page in the currently known urls."
   (with-local-parsing-environment
-    (when (string= (first route) (first url-sections))
+    (when (cl-ppcre:scan (first route) (first url-sections))
       ;; moving to an iterative solution
       (maplist
        (lambda (unhandled-routing-directives)
@@ -87,38 +154,55 @@
 		    (when handler
 		      (return-from search-handler handler))))
 		 ((and (listp item) (eq (first item) 'handler))
-		  (apply (second item) (first url-sections) (rest (rest item)))
+		  (apply (funcall (second item) :to-page) (first url-sections) (rest (rest item)))
 		  (util:return-when search-handler (search-handler `(,(first route) ,@(rest unhandled-routing-directives)) url-sections))))))
        (rest route)))))
 
 (defun handler-url (page &rest options)
   "Finds the url for a given page-handler.  The options can specify any number of (currently unspecified options)"
   (dolist (route *ROUTING-TABLE*)
-    (let ((url (apply 'search-url route page options)))
+    (let ((url (apply 'search-url route page nil options)))
       (when url
-	(return-from handler-url url)))))
+	(return-from handler-url (format nil "~{~a~^/~}" (reverse url)))))))
 
-(defun search-url (route page &rest options)
+(defun search-url (route page url-sections &rest options)
   "Searches for a url of the given page, for the current (sub) route."
-  (if (eql (second route) page)
-      (first route)
-      (dolist (subroute (rest route))
-	(when (listp subroute)
-	  (let ((url (apply 'search-url subroute page options)))
-	    (when url
-	      (return-from search-url (concatenate 'string (first route) (string +URLSPLIT+) url))))))))
+  (let ((url-sections (cons (first route) url-sections))) ;; last part of the url is the first item in this list
+    (dolist (item (rest route))
+      (cond ((or (symbolp item) (functionp item))
+	     (when (eql page item)
+	       (return-from search-url url-sections)))
+	    ((and (listp item) (stringp (first item)))
+	     (util:return-when search-url (apply 'search-url item page url-sections options)))
+	    ((and (listp item) (eq (first item) 'handler))
+	     (let ((new-url-sections (apply (funcall (second item) :to-url) url-sections options (rest (rest item)))))
+	       (if new-url-sections
+		   (setf url-sections new-url-sections)
+		   (return-from search-url nil))))))))
 
-(defun sets (url-part variable)
-  "Handler sets ::
-     Sets the currently matched url-part to the given variable."
-  (declare (special url-variables))
-  (push (cons variable url-part) url-variables))
+(defmacro defhandler (name documentation ((&rest to-page-args) &body to-page-body) ((&rest to-url-args) &body to-url-body))
+  `(defun ,name (dir)
+     ,documentation
+     (cond ((eql dir :to-page)
+	    (lambda ,to-page-args
+	      ,@to-page-body))
+	   ((eql dir :to-url)
+	    (lambda ,to-url-args
+	      ,@to-url-body)))))
+
+(defhandler sets
+    "Sets the currently matched url-part to the given variable"
+  ((url-part variable)
+   (declare (special url-variables))
+   (push (cons variable url-part) url-variables))
+  ((url-sections options variable)
+   (when (getf options variable)
+     (cons (getf options variable) (rest url-sections)))))
 
 (defun url-var (variable)
   "Returns the value of the variable found in the url."
   (cdr (assoc variable *url-variables*)))
 	   
-		 
 ;; (set-routing-table
 ;;  ("admin"
 ;;   'site.admin:overview
